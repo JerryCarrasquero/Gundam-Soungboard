950f30ebba15b8b0a8195ffe3467dc4101a35028
JerryCarrasquero-code <carrasquerojerry@gmail.com>
2025-09-08 21:20:20 -0400

    [Develop] toolbar logo

--- lib/theming/soundboard_toolbar.dart
+++ lib/theming/soundboard_toolbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SoundboardToolbar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
@@ -12,9 +13,27 @@ class SoundboardToolbar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(title),
-      actions: actions,
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        AppBar(
+          backgroundColor: Theme.of(context).colorScheme.primary,
+          title: Row(
+            children: [
+              SvgPicture.asset(
+                'assets/svgLogo/Earth_Federation_Forces_emblem.svg',
+                height: 32,
+                width: 32,
+                colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+              ),
+              const SizedBox(width: 8),
+              Text(title),
+            ],
+          ),
+          actions: actions,
+          elevation: 0,
+        ),
+      ],
     );
   }
 
